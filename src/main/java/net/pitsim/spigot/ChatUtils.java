b1c910392745534b2e1a49e78109597c61b92900
Logan <tonierbobcat966@gmail.com>
2025-02-10 12:11:28 -0800

    Update ChatUtils.java

--- src/main/java/net/pitsim/spigot/ChatUtils.java
+++ src/main/java/net/pitsim/spigot/ChatUtils.java
@@ -3,13 +3,19 @@ package net.pitsim.spigot;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import java.util.logging.Level;
 
 public class ChatUtils {
     public static String parse(String message, Player papiTarget) {
         return parse(PlaceholderAPI.setPlaceholders(papiTarget, message));
     }
     public static String parse(String message) {
-        return LegacyComponentSerializer.legacyAmpersand().serialize(Component.text(message));
+        Bukkit.getLogger().log(Level.INFO, "Parsing: " + message + "...");
+        String result = LegacyComponentSerializer.legacyAmpersand().serialize(Component.text(message));
+        Bukkit.getLogger().log(Level.INFO, "Result: " + result + "!");
+        return result;
     }
 }
